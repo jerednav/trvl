27d2e37e71bfda6a2eb9299cf9711db13c0238de
jerednav <80506833+jerednav@users.noreply.github.com>
2022-08-14 22:19:32 -0700

    adding userlist&useritem page/component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>User works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Jered",
+      image:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.9pCH9Vse1lq2vwEsVUpskgHaH2%26pid%3DApi&f=1",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
