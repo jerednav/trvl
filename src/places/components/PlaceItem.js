9e72f5096b07d743255a2439316f7d2bdbcdfebf
jerednav <80506833+jerednav@users.noreply.github.com>
2022-08-17 22:38:45 -0700

    adding google maps to map modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -4,6 +4,7 @@ import "./PlaceItem.css";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
@@ -22,7 +23,9 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>Close</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP!</h2>
+          <h2>
+            <Map center={props.coordinates} zoom={16} />
+          </h2>
         </div>
       </Modal>
       <li className="place-item">
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -6,10 +6,10 @@ import PlaceList from "../components/PlaceList";
 const DUMMY_PLACES = [
   {
     id: "p1",
-    title: "Empire State BUilding",
-    description: "ONe of the most famous sky scrapers in the world",
-    image:
-      "https://www.tripsavvy.com/thmb/sBI2W7YNV4vRSVdbRVfASLH3F6I=/2617x3874/filters:fill(auto,1)/5891665274_cc93622eb7_o-56a3ff3b5f9b58b7d0d4df13.jpg",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
     address: "20 W 34th St, New York, NY 10001",
     location: {
       lat: 40.7484405,
@@ -19,10 +19,10 @@ const DUMMY_PLACES = [
   },
   {
     id: "p2",
-    title: "Empire State BUilding",
-    description: "ONe of the most famous sky scrapers in the world",
-    image:
-      "https://www.tripsavvy.com/thmb/sBI2W7YNV4vRSVdbRVfASLH3F6I=/2617x3874/filters:fill(auto,1)/5891665274_cc93622eb7_o-56a3ff3b5f9b58b7d0d4df13.jpg",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
     address: "20 W 34th St, New York, NY 10001",
     location: {
       lat: 40.7484405,
@@ -37,4 +37,5 @@ const UserPlaces = () => {
   const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
   return <PlaceList items={loadedPlaces} />;
 };
+
 export default UserPlaces;
