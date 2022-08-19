ec6eae3d3ee9528e69ac43836d6f533f6c19ca1b
Jered <jerednav@gmail.com>
2022-08-18 22:40:13 -0700

    adding form component foundation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" />
+      <Input element="input" type="text" label="Title" />
     </form>
   );
 };
